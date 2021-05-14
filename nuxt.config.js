243a062404b05ca920efa912c6a13e50ed0ab4b5
morethanmin <mini4614@gmail.com>
2021-05-14 15:15:41 +0900

    feat:타이틀 및 로고 수정

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,8 @@ import colors from "vuetify/es5/util/colors";
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: "%s - til-hub",
-    title: "til-hub",
+    titleTemplate: "%s",
+    title: "cat-hub",
     htmlAttrs: {
       lang: "en"
     },
