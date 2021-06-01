2e235fffb3ca3bef1969f906796a3710cc3c277a
morethanmin <mini4614@gmail.com>
2021-06-02 00:08:39 +0900

    fix: route error

--- components/ActivityBox.vue
+++ components/ActivityBox.vue
@@ -4,12 +4,7 @@
       <v-icon v-if="icon !== ``" class="title-icon">
         {{ icon }}
       </v-icon>
-      <img
-        v-if="img !== ``"
-        :src="`/cat-hub/_nuxt/${img}`"
-        alt=""
-        class="title-icon"
-      />
+      <img v-if="img !== ``" :src="img" alt="" class="title-icon" />
       <div class="title-text">
         {{ title }}
       </div>
