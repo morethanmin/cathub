377b4ae0706b431fbddfffac0956e0aa739f7935
morethanmin <mini4614@gmail.com>
2021-06-08 15:37:25 +0900

    add recomendation

--- components/CommentBoxList.vue
+++ components/CommentBoxList.vue
@@ -1,28 +1,28 @@
 <template>
   <div>
-    <comment-box
+    <!-- <comment-box
       :img="require('~/static/images/user.png')"
       title="안도현"
-      subTitle="giant frontend-developer"
+      subTitle="Giant frontend-developer"
       comment="이 개발자를 추천합니다. 테스트 글입니다.이 개발자를 추천합니다. 테스트 글입니다.이 개발자를 추천합니다. 테스트 글입니다.이 개발자를 추천합니다. 테스트 글입니다.이 개발자를 추천합니다. 테스트 글입니다."
     />
     <comment-box
       :img="require('~/static/images/user.png')"
       title="송정환"
-      subTitle="giant server-developer"
+      subTitle="Giant server-developer"
       comment="이 개발자를 추천합니다. 테스트 글입니다."
-    />
+    /> -->
     <comment-box
       :img="require('~/static/images/user.png')"
       title="Jay"
       subTitle="Weplanet frontend-developer"
-      comment="이 개발자를 추천합니다. 테스트 글입니다."
+      comment="Jade는 개발이라는 업무를 일로 생각하지않고 즐기는 개발자였어요. 항상 현재에 안주하지 않고 끊임없이 성장하는 모습을 보면 찐 개발자라고 생각하면서도 다양한 부분에서 뛰어난 감각을 가지고 있는 사람이여서 잠깐이였지만 팀에 정말 많은 영향을 주었어요. 다재다능한 개발자를 찾으신다면 추천드립니다!"
     />
     <comment-box
       :img="require('~/static/images/user.png')"
       title="이성현"
-      subTitle="Trigit-Software development team leader"
-      comment="이 개발자를 추천합니다. 테스트 글입니다."
+      subTitle="Trigit-Software development leader"
+      comment="상민님과 짧은 기간동안 협업을 하면서 느꼈던 점은 개발에 대한 높은 관심과 빠른 습득력을 가진 개발자라는것입니다. 짦은 기간 인턴으로 함께한다는 것이 아쉬울 정도로 커뮤니티 능력, 개발 능력에 있어서 높은 가능성을 보여주었습니다."
     />
   </div>
 </template>
